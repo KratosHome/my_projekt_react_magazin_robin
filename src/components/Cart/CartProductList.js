8d76f226324e47df13c8093bd78ab7e916ad7f9d
Kratoshome <kratoshome101@gmail.com>
2021-11-14 19:00:31 +0200

    fsvfsv

--- src/components/Cart/CartProductList.js
+++ src/components/Cart/CartProductList.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { keys } from "lodash";
+import productsArray, {
+  getProductsObject,
+} from "../ProducctsList/productsArray";
 
-const CardProductList = ({}) => {
-  return <></>;
+const CardProductList = ({
+  productsInCard,
+  productsObject = getProductsObject(productsArray),
+  CartItem,
+  changeProductQuantity,
+}) => {
+  return (
+    <>
+      {keys(productsInCard).map((productId) => (
+        <CartItem
+          key={productId}
+          product={productsObject[productId]}
+          productCount={productsInCard[productId]}
+          changeProductQuantity={changeProductQuantity}
+        />
+      ))}
+    </>
+  );
 };
 export default CardProductList;
